ec0477f92157dab6eed3c79a7b94c2372c9f1414
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2025-12-21 01:05:41 +0100

    Remove redundant imports of `LoggerJvm` and `LoggerNative` in platform-specific logging implementations

--- ktorlib/src/nativeMain/kotlin/io/github/smyrgeorge/ktorlib/util/logging.native.kt
+++ ktorlib/src/nativeMain/kotlin/io/github/smyrgeorge/ktorlib/util/logging.native.kt
@@ -1,6 +1,5 @@
 package io.github.smyrgeorge.ktorlib.util
 
-import io.github.smyrgeorge.ktorlib.util.LoggerNative
 import io.ktor.util.logging.*
 
 actual fun applicationLogger(name: String): Logger = LoggerNative(name)

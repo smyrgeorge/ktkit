7c2224330baf2768339d6136b80cd723a44cc8d3
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2026-01-06 12:56:14 +0100

    Add documentation for `EventContext` and `ConfigPropertiesToml`, update package path for `ConfigPropertiesToml`

--- ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/api/event/EventContext.kt
+++ ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/api/event/EventContext.kt
@@ -2,6 +2,12 @@ package io.github.smyrgeorge.ktkit.api.event
 
 import io.github.smyrgeorge.ktkit.context.UserToken
 
+/**
+ * Represents the context of an event that includes authentication and request metadata.
+ *
+ * @property user The authenticated user's token containing identity and metadata information.
+ * @property headers A map of request headers, providing additional context or metadata about the event.
+ */
 class EventContext(
     val user: UserToken,
     val headers: Map<String, String>
--- ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/api/props/ConfigPropertiesToml.kt
+++ ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/api/props/ConfigPropertiesToml.kt
@@ -1,10 +1,16 @@
-package io.github.smyrgeorge.ktkit.config
+package io.github.smyrgeorge.ktkit.api.props
 
 import com.akuleshov7.ktoml.Toml
 import io.github.smyrgeorge.ktkit.util.readEntireFileUtf8
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.Serializable
 
+/**
+ * Utility object for loading and deserializing configuration properties from TOML files.
+ *
+ * Provides a method for reading TOML files and deserializing their content into strongly typed objects
+ * using Kotlin serialization.
+ */
 object ConfigPropertiesToml {
     inline fun <reified T : @Serializable Any> loadFromFile(
         deserializer: DeserializationStrategy<T>,

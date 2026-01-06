8b506398c426aff90bcbb0f2f259d2ab4afe6c50
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2026-01-06 13:49:47 +0100

    Add `app` property to `AbstractComponent` for global access to `Application.INSTANCE`, update documentation accordingly

--- ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/service/AbstractComponent.kt
+++ ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/service/AbstractComponent.kt
@@ -1,5 +1,6 @@
 package io.github.smyrgeorge.ktkit.service
 
+import io.github.smyrgeorge.ktkit.Application
 import io.github.smyrgeorge.ktkit.context.ExecutionContext
 import kotlinx.coroutines.currentCoroutineContext
 
@@ -15,6 +16,23 @@ import kotlinx.coroutines.currentCoroutineContext
  * an [ExecutionContext] within the current coroutine context.
  */
 interface AbstractComponent {
+    /**
+     * Provides access to the singleton instance of the [Application].
+     *
+     * This property is a global accessor to the application's main instance ([Application.INSTANCE]).
+     * It is used to interact with or retrieve the configuration, state, or other resources
+     * managed by the [Application] class. By design, it ensures that only one instance of the
+     * application exists during the runtime.
+     *
+     * This is particularly useful in scenarios where shared access to the [Application] is
+     * required across multiple components such as services or handlers that implement or extend
+     * [AbstractComponent] or similar abstractions.
+     *
+     * Note: Ensure to initialize the application properly before accessing this property to avoid
+     * runtime errors when the underlying instance is unavailable.
+     */
+    val app: Application get() = Application.INSTANCE
+
     /**
      * Retrieves the [io.github.smyrgeorge.ktkit.context.ExecutionContext] from the current CoroutineContext.
      *

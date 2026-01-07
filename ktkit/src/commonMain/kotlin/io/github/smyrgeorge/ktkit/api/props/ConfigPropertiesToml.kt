1fc54bde864ccfdb6ee1eda5d06ad64dada7e0f7
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2026-01-07 17:56:36 +0100

    Refactor file loading: introduce `Path`-based methods, extract `loadFile` for reusable TOML deserialization, and update `readEntireFileUtf8` signature.

--- ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/api/props/ConfigPropertiesToml.kt
+++ ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/api/props/ConfigPropertiesToml.kt
@@ -2,6 +2,7 @@ package io.github.smyrgeorge.ktkit.api.props
 
 import com.akuleshov7.ktoml.Toml
 import io.github.smyrgeorge.ktkit.util.readEntireFileUtf8
+import kotlinx.io.files.Path
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.Serializable
 
@@ -22,11 +23,41 @@ object ConfigPropertiesToml {
      * @param path The path to the file that will be read and deserialized.
      * @return An instance of type `T` created from the file's contents.
      */
-    inline fun <reified T : @Serializable Any> loadFromFile(
+    inline fun <reified T : @Serializable Any> loadFrom(
         deserializer: DeserializationStrategy<T>,
         path: String
+    ): T = loadFrom(deserializer, Path(path))
+
+    /**
+     * Loads and deserializes a file's contents into an object of type `T`.
+     *
+     * The file is read as UTF-8, and its contents are deserialized using the provided `deserializer`
+     * and `TOML` format.
+     *
+     * @param deserializer The deserialization strategy used to interpret the contents of the file.
+     * @param path The path to the file that will be read and deserialized.
+     * @return An instance of type `T` created from the file's contents.
+     */
+    inline fun <reified T : @Serializable Any> loadFrom(
+        deserializer: DeserializationStrategy<T>,
+        path: Path
     ): T {
         val file = readEntireFileUtf8(path)
-        return Toml.decodeFromString(deserializer, file)
+        return loadFile(deserializer, file)
     }
+
+    /**
+     * Loads and deserializes a file's contents into an object of type `T`.
+     *
+     * The file is read as UTF-8, and its contents are deserialized using the provided `deserializer`
+     * and `TOML` format.
+     *
+     * @param deserializer The deserialization strategy used to interpret the contents of the file.
+     * @param content The contents of the file to be deserialized.
+     * @return An instance of type `T` created from the file's contents.
+     */
+    inline fun <reified T : @Serializable Any> loadFile(
+        deserializer: DeserializationStrategy<T>,
+        content: String
+    ): T = Toml.decodeFromString(deserializer, content)
 }
--- ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/util/file.kt
+++ ktkit/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/util/file.kt
@@ -7,9 +7,9 @@ import kotlinx.io.readByteArray
 
 private val fs = SystemFileSystem
 
-fun readEntireFileUtf8(path: String): String {
+fun readEntireFileUtf8(path: Path): String {
     val buffer = Buffer()
-    val source = fs.source(Path(path))
+    val source = fs.source(path)
     try {
         source.readAtMostTo(buffer, Int.MAX_VALUE.toLong())
         return buffer.readByteArray().decodeToString() // UTF-8 by default

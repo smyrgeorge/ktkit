c947a44ee33c690a656fda876ee5267cc00c12a8
Yorgos Smyrnaios <smyrgeorge@gmail.com>
2026-01-15 01:05:52 +0100

    Enable snake_case support in default JSON configuration and improve documentation for `JsonSupport`.

--- ktkit-sqlx4k/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/sqlx4k/JsonSupport.kt
+++ ktkit-sqlx4k/src/commonMain/kotlin/io/github/smyrgeorge/ktkit/sqlx4k/JsonSupport.kt
@@ -6,14 +6,32 @@ import io.github.smyrgeorge.ktkit.util.default
 import io.github.smyrgeorge.sqlx4k.ResultSet
 import io.github.smyrgeorge.sqlx4k.ValueEncoder
 import io.github.smyrgeorge.sqlx4k.ValueEncoderRegistry
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNamingStrategy
 import kotlinx.serialization.serializer
 import kotlin.reflect.KClass
 
+/**
+ * Utility object for providing JSON serialization and deserialization support,
+ * including the creation of value encoders for mapping Kotlin classes to string
+ * representations suitable for database interactions.
+ *
+ * This object simplifies the registration and usage of JSON-based encoders
+ * by offering methods for generating value encoder registries for specified
+ * Kotlin classes. It supports a customizable JSON configuration through the
+ * kotlinx.serialization library.
+ */
 object JsonSupport {
-    private val defaultJson = Json { default() }
+    @OptIn(ExperimentalSerializationApi::class)
+    private val defaultJson by lazy {
+        Json {
+            default()
+            namingStrategy = JsonNamingStrategy.SnakeCase
+        }
+    }
 
     fun encoders(vararg types: KClass<*>): ValueEncoderRegistry =
         encoders(defaultJson, *types)
